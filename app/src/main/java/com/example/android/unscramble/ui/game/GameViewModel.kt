efc7532dc3ec01d545341fb95b7077144336a4f1
татьяна <emuart155@gmail.com>
2021-08-22 20:52:48 +0300

    В этой задаче мы дополнительно заполняем GameViewModel вспомогательные методы для получения следующего слова, проверки слова игрока для увеличения счета и проверки количества слов для завершения игры.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel()  {
@@ -13,7 +14,54 @@ class GameViewModel : ViewModel()  {
     // Контроллер пользовательского интерфейса GameFragment может только считывать значение
     // с помощью public свойства currentScrambledWord,
     // доступного только для чтения
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    init {
+        Log.d("GameFragment", "GameViewModel создался!")
+        getNextWord()
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        //преобразуйте строку currentWord в массив символов
+        val tempWord = currentWord.toCharArray()
+        //перемешаем буквы
+        tempWord.shuffle()
+        //Иногда перетасованный порядок символов совпадает с исходным словом.
+        // Добавьте следующий while цикл вокруг вызова перемешивания, чтобы продолжить цикл до тех пор,
+        // пока зашифрованное слово не станет таким же, как исходное слово.
+        while (tempWord.toString().equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        //Добавьте if-else блок, чтобы проверить, использовалось ли уже слово. Если wordsList содержит currentWord,
+        // вызовите getNextWord(). Если нет, обновите значение _currentScrambledWord с помощью нового
+        // зашифрованного слова, увеличьте количество слов и добавьте новое слово в wordsList.
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    /*
+* Возвращает true, если текущее количество слов меньше MAX_NO_OF_WORDS.
+* Обновляет следующее слово.
+*/
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel уничтожилась!")
+    }
 }
